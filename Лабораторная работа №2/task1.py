bd2cda75e9d5cfe4b7147b898b3b68a98adaf879
Alina Romanova <144436940+roomaaanovva@users.noreply.github.com>
2024-12-20 21:41:46 +0300

    Update task1.py

--- Лабораторная работа №2/task1.py
+++ Лабораторная работа №2/task1.py
@@ -27,8 +27,8 @@ BOOKS_DATABASE = [
 if __name__ == '__main__':
     # инициализируем список книг
     list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], \
-             pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"])\
+        for book_dict in BOOKS_DATABASE
     ]
     for book in list_books:
         print(book)  # проверяем метод str
